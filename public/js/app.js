7205f18ff32a3e9b9c9be5c56f7752c323809682
anca <anca.grosan@gmail.com>
2013-09-07 22:48:31 +0300

    the very important functionality

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,16 @@ $(function() {
 	 $('#upload-form').submit();
    });
 });
+
+$("#r1, #r2, #r3, #r4, #r5, #r6").change(function () {
+  $(this).parent().parent().addClass("hide");
+  $("#loading").removeClass("hide");
+
+  setTimeout(function(){
+	$("#loading").addClass("hide");
+	$("#after").removeClass("hide");
+
+	$('#r4').attr('checked', false);
+	$('#r5').attr('checked', false);
+  },2000);
+});
